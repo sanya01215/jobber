ed97f867e6ae9907ba3a9226366dd92f92b24f1c
sanya01215 <siabruk.o@gmail.com>
2023-03-07 19:31:45 +0200

    Add example oauth0 autorization

---
+++ src/main/java/com/siabgroup/Jobber/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.siabgroup.Jobber.controller;
+
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for the home page.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "home";
+    }
+}
